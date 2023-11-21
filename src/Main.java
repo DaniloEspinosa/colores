8099363fd1f25b2722c59d031169f23d92e1cefc
Danilo <danilopespinosa@gmail.com>
2023-11-21 13:00:27 +0100

    Primer commit en JAVA Listado de colores.

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Lista de colores");
+        System.out.println("¿Cuantos colores quieres introducir?");
+        int numColores = input.nextInt();
+
+        ArrayList<String> colores = new ArrayList<String>();
+
+
+        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1 ; i <= numColores; i++) {
+            System.out.println("Introduce el color nº" + i + ":");
+            String color = input.next();
+            colores.add(color);
+        }
+
+        System.out.println("Has introducido los siguientes colores: ");
+
+        for (int i = 0; i < colores.size(); i++) {
+            System.out.printf( "\t" + colores.get(i));
+        }
+
+        System.out.println("\n" + colores.toString());
+
+        input.close();
+    }
+}
